630ceb06af2fdb17b717c4e4062cec74318f42c9
ravisdraw <ravikumar11.04.1998@gmail.com>
2024-04-18 07:12:32 +0530

    dashboard changes

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import {MatExpansionModule} from '@angular/material/expansion';
+
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MatExpansionModule,
   ]
 })
 export class DashboardModule { }
--- src/app/components/dashboard/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ClipboardService } from 'ngx-clipboard';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  panelOpenState: boolean = false;
 
+  filters: any[] = ['AI', 'Tech', 'Love'];
+  instaID = '';
+  instaUrl: SafeResourceUrl = '';
+  posts: any[] = [];
+
+  commonURL = 'https://www.instagram.com/p/Ctq5NkItF7A/embed/';
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private clipboardService: ClipboardService
+  ) {}
+
+  ngOnInit(): void {}
+
+  sanitizeUrl(): SafeResourceUrl {
+    var url = `https://www.instagram.com/p/${this.instaID}/embed/`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  showInstagramPost(id: string) {
+    this.instaID = id;
+    this.instaUrl = this.sanitizeUrl();
+    this.panelOpenState = true;
+  }
+
+  goToInstagramPost(url: string) {
+    window.open(url, 'blank');
+  }
+
+  goToEditPost(obj: any) {
+    // this.router.navigate(['/edit-post'], {queryParams: { data: JSON.stringify(obj)}});
+  }
+
+  copyToClipboard(url: string) {
+    this.clipboardService.copy(url);
+    alert('Post URL Copied to Clipboard!');
+  }
+
+  deletePost(id: string) {
+    this.posts = this.posts.filter((item) => item.id !== id);
+  }
 }
